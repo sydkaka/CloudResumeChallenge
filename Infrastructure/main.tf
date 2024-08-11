7dbb36b5f4cdbcbdbe019cd3ab645c225dcf1b21
yongdingsun <yongdingsunchi@outlook.com>
2024-08-10 18:02:33 -0700

    fix

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "azurerm" {
-      resource_group_name  = "Personal"
-      storage_account_name = "ydsunstorageaccount"
-      container_name       = "$web"
-      key                  = "terraform.tfstate"
+    resource_group_name  = "Personal"
+    storage_account_name = "ydsunstorageaccount"
+    container_name       = "$web"
+    key                  = "terraform.tfstate"
   }
   required_version = ">= 1.1.0"
 }
@@ -25,15 +25,15 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storageaccount" {
-  name                     = "ydsunstorageaccount"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  account_kind = "StorageV2"
-  access_tier = "Hot"
-  min_tls_version = "TLS1_2"
-  allow_nested_items_to_be_public = false
+  name                             = "ydsunstorageaccount"
+  resource_group_name              = azurerm_resource_group.rg.name
+  location                         = azurerm_resource_group.rg.location
+  account_tier                     = "Standard"
+  account_replication_type         = "LRS"
+  account_kind                     = "StorageV2"
+  access_tier                      = "Hot"
+  min_tls_version                  = "TLS1_2"
+  allow_nested_items_to_be_public  = false
   cross_tenant_replication_enabled = false
   static_website {
     index_document = "index.html"
